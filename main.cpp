cb2b9b0c47daeb207c5cd648a66421aa84e63b25
ShunPeng1 <shunpeng1@gmail.com>
2022-10-28 15:21:00 +0700

    reverse substring testcase

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include "ConcatStringTree.h"
 
+#define CST ConcatStringTree
 void tc11(){
     
-    ConcatStringTree s1("Hello"), s2("World");
+    CST s1("Hello"), s2("World");
     cout<<s1.get(0)<<endl;
     cout<<s1.get(1)<<endl;
     cout<<s1.get(2)<<endl;
@@ -17,7 +18,7 @@ void tc11(){
     cout<<s1.toStringPreOrder()<<endl;
     cout<<s1.toString()<<endl;
 
-    ConcatStringTree s3 = s1.concat(s2);
+    CST s3 = s1.concat(s2);
     cout<<endl<<endl;
     cout<<s3.get(5)<<endl;
     cout<<s3.get(6)<<endl;
@@ -38,7 +39,7 @@ void tc11(){
 }
 void tc12(){
     
-    ConcatStringTree s1("Hello"), s2("World");
+    CST s1("Hello"), s2("World");
     cout<<s1.get(0)<<endl;
     cout<<s1.get(1)<<endl;
     cout<<s1.get(2)<<endl;
@@ -53,7 +54,7 @@ void tc12(){
     cout<<s1.toStringPreOrder()<<endl;
     cout<<s1.toString()<<endl;
 
-    ConcatStringTree s3 = s1.concat(s2);
+    CST s3 = s1.concat(s2);
     cout<<endl<<endl;
     cout<<s3.get(5)<<endl;
     cout<<s3.get(6)<<endl;
@@ -70,53 +71,38 @@ void tc12(){
     cout<<s3.toString()<<endl;
 
     cout<<endl<<endl;
+
+    CST s4 = s3.subString(1,9);
+    CST s6 = s3.subString(1,4);
+    cout<<"s4 = "<<s4.toStringPreOrder()<<endl;
+    cout<<"s6 = "<<s6.toStringPreOrder()<<endl;
+
+    CST s5 = s4.reverse();
+    CST s7 = s6.reverse();
+    cout<<"s5 = "<<s5.toStringPreOrder()<<endl;
+    cout<<"s7 = "<<s7.toStringPreOrder()<<endl;
+
+}
+
+void tc17(){
+    CST sA("Hello"), sB(",_t"), sC("his_is"), sD("_an");
+    CST s1 = sA.concat(sB), s2 = sC.concat(sD), s3 = s1.concat(s2);
+
+    
+    cout<<"s1 = "<<s1.toStringPreOrder()<<endl;
+    cout<<"s2 = "<<s2.toStringPreOrder()<<endl;    
+    cout<<"s3 = "<<s3.toStringPreOrder()<<endl;
+    
+    CST s4 = s3.subString(1,10);
+    
+    cout<<"s4 = "<<s4.toStringPreOrder()<<endl;
+
+    CST s5 = s4.reverse();
+    cout<<"s5 = "<<s5.toStringPreOrder()<<endl;
 
 }
 
 int main() {
-    
-    ConcatStringTree s1("Hello"), s2("World");
-    cout<<s1.get(0)<<endl;
-    cout<<s1.get(1)<<endl;
-    cout<<s1.get(2)<<endl;
-    cout<<s1.get(3)<<endl;
-    cout<<s1.get(4)<<endl;
-    cout<<s1.indexOf('H')<<endl;
-    cout<<s1.indexOf('e')<<endl;
-    cout<<s1.indexOf('l')<<endl;
-    cout<<s1.indexOf('l')<<endl;
-    cout<<s1.indexOf('o')<<endl;
-    cout<<s1.indexOf('a')<<endl;
-    cout<<s1.toStringPreOrder()<<endl;
-    cout<<s1.toString()<<endl;
-
-    ConcatStringTree s3 = s1.concat(s2);
-    cout<<endl<<endl;
-    cout<<s3.get(5)<<endl;
-    cout<<s3.get(6)<<endl;
-    cout<<s3.get(7)<<endl;
-    cout<<s3.get(8)<<endl;
-    cout<<s3.get(9)<<endl;
-    cout<<s3.indexOf('W')<<endl;
-    cout<<s3.indexOf('o')<<endl;
-    cout<<s3.indexOf('r')<<endl;
-    cout<<s3.indexOf('l')<<endl;
-    cout<<s3.indexOf('d')<<endl;
-    cout<<s3.indexOf('a')<<endl;
-    cout<<s3.toStringPreOrder()<<endl;
-    cout<<s3.toString()<<endl;
-
-    cout<<endl<<endl;
-
-    ConcatStringTree s4 = s3.subString(1,9);
-    ConcatStringTree s6 = s3.subString(1,4);
-    cout<<"s4 = "<<s4.toStringPreOrder()<<endl;
-    cout<<"s6 = "<<s6.toStringPreOrder()<<endl;
-
-    ConcatStringTree s5 = s4.reverse();
-    ConcatStringTree s7 = s6.reverse();
-    cout<<"s5 = "<<s5.toStringPreOrder()<<endl;
-    cout<<"s7 = "<<s7.toStringPreOrder()<<endl;
-
+    tc17();
     return 0;
 }
